0d147a8c9c843ee27c71ccec2cfeff59f3106832
Mark <markkong30@gmail.com>
2021-12-08 18:23:29 +0800

    second save

--- js/application.js
+++ js/application.js
@@ -1,10 +1,18 @@
-
 var subTotalPrice = function (ele) {
   var price = parseFloat($(ele).find('.price').text());
-  var quantity = parseInt($(ele).find('.quantity input').val());
+  var quantity;
+  if ($(ele).find('.quantity input').val().length == 0) {
+    quantity = 0;
+  } else {
+    quantity = parseInt($(ele).find('.quantity input').val());
+  }
+
 
   var subTotal = price * quantity;
-  $(ele).find('.subtotal').html(subTotal);
+  
+    $(ele).find('.subtotal').html(subTotal);
+  
+
   return subTotal;
 };
 
@@ -20,6 +28,16 @@ var updateTotal = function () {
 
   $('#totalDisplay').html(sum);
 };
+var addContent1 = function () {
+var name = $('#addName').val();
+var price = $('#addPrice').val();
+var qty = $('#addQty').val();
+var addContent = 
+  '<tr><td class="name">'+ name + '</td>' + '<td>$<span class="price">' + price +'</span></td>' + '<td class="quantity">QTY <input type="number" value="' + qty + '"/></td>' + '<td><button class="btn btn-sm btn-dark remove">Remove</button></td>' + '<td>$<span class="subtotal">--.--</span></td></tr>';   
+  return addContent;
+}
+
+
 
 
 
@@ -28,10 +46,18 @@ $(document).ready(function () {
 
 
 $('body').on('input','tr input', function (){
-  
-})
+  updateTotal();
+});
 
+$('body').on('click','button.remove', function () {
+  $(this).closest('tr').remove();
+  updateTotal();
+});
 
+$('body').on('click', 'button.add', function () {
+  $('tbody').prepend(addContent1());
+  updateTotal();
+});
 
 
 
