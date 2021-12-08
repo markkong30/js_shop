f508f7e9857e2f1316f6f090399a166e200d0480
Mark <markkong30@gmail.com>
2021-12-08 19:40:18 +0800

    third save

--- js/application.js
+++ js/application.js
@@ -1,18 +1,16 @@
 var subTotalPrice = function (ele) {
   var price = parseFloat($(ele).find('.price').text());
-  var quantity;
-  if ($(ele).find('.quantity input').val().length == 0) {
+  var quantity = $(ele).find('.quantity input').val();
+  if (quantity.length == 0 || quantity < 0) {
     quantity = 0;
   } else {
-    quantity = parseInt($(ele).find('.quantity input').val());
-  }
-
+    quantity = parseInt($(ele).find('.quantity input').val()
+    );
+  };
 
   var subTotal = price * quantity;
+  $(ele).find('.subtotal').html(subTotal);
   
-    $(ele).find('.subtotal').html(subTotal);
-  
-
   return subTotal;
 };
 
@@ -22,20 +20,29 @@ var updateTotal = function () {
   $('tbody tr').each(function (i ,ele) {
     totalPrice.push(subTotalPrice(ele));
   });
-  var sum = totalPrice.reduce(function (a, b) {
+  var sumDec = totalPrice.reduce(function (a, b) {
     return a + b;
   });
+  var sum = sumDec.toFixed(2);
 
   $('#totalDisplay').html(sum);
 };
-var addContent1 = function () {
-var name = $('#addName').val();
-var price = $('#addPrice').val();
-var qty = $('#addQty').val();
-var addContent = 
-  '<tr><td class="name">'+ name + '</td>' + '<td>$<span class="price">' + price +'</span></td>' + '<td class="quantity">QTY <input type="number" value="' + qty + '"/></td>' + '<td><button class="btn btn-sm btn-dark remove">Remove</button></td>' + '<td>$<span class="subtotal">--.--</span></td></tr>';   
-  return addContent;
-}
+
+var addContent = function () {
+  var name = $('#addName').val();
+  var price = $('#addPrice').val();
+  var qty = $('#addQty').val();
+  if (name.length == 0 || price.length == 0 || price < 0 || qty < 0 || qty.indexOf('.') !== -1) {
+    return;
+  };
+
+  var priceDisplay = Number(price).toFixed(2);
+
+  var content = 
+    '<tr><td class="name">'+ name + '</td>' + '<td>$<span class="price">' + priceDisplay +'</span></td>' + '<td class="quantity">QTY <input type="number" min="0" value="' + qty + '"/></td>' + '<td><button class="btn btn-sm btn-dark remove">Remove</button></td>' + '<td>$<span class="subtotal">--.--</span></td></tr>';  
+
+  return content;
+};
 
 
 
@@ -44,22 +51,18 @@ var addContent =
 $(document).ready(function () {
   updateTotal();
 
+  $('body').on('input','tr input', function (){
+    updateTotal();
+  });
 
-$('body').on('input','tr input', function (){
-  updateTotal();
-});
+  $('body').on('click','button.remove', function () {
+    $(this).closest('tr').remove();
+    updateTotal();
+  });
 
-$('body').on('click','button.remove', function () {
-  $(this).closest('tr').remove();
-  updateTotal();
-});
-
-$('body').on('click', 'button.add', function () {
-  $('tbody').prepend(addContent1());
-  updateTotal();
-});
-
-
-
-
+  $('body').on('submit', '#addItem', function (event) {
+    event.preventDefault();
+    $('tbody').prepend(addContent());
+    updateTotal();
+  });
 });
